e80cec7397a512d1a3648c518d575f1ab3907bdc
Wesley Garreto <wesley_garreto@hotmail.com>
2021-12-06 19:19:51 -0300

    Backend initial structure

---
+++ service/src/test/java/com/garreto/client_app/ClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garreto.client_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
